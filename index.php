baf8fce00a03104aa9108b34e2ea74427573359a
Atsushi <>
2018-07-21 20:40:58 +0800

    dbへの接続情報追加

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
-$daysInJapanese = ["日", "月", "火", "水", "木", "金", "土"];
+    $dsn = 'mysql:dbname='. getenv('DB_NAME') .';host=' . getenv('HOST_NAME');
+    $user = getenv('SQL_USER');
+    $password = getenv('SQL_PASSWORD');
+    $dbh = new PDO($dsn, $user, $password);
+    // SQL文にエラーがあった際、画面にエラーを出力する設定
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->query('SET NAMES utf8');
 
-parse_str($_SERVER['QUERY_STRING'], $queryString);
-$targetDate = strtotime($queryString['date']);
-$dayInJapanese = $daysInJapanese[date("w", $targetDate)] . "曜日";
- 
-$response = array(
-    'target_date' => $targetDate,
-    'day_in_japanese' => $dayInJapanese
-);
- 
-echo json_encode($response);
+    $sql = 'SELECT * FROM `test`';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    echo json_encode($record);
 
 
 // curl -X GET "127.0.0.1:8888/index.php?date=2018%2F07%2F17";
+// curl -X GET "https://web-api-sample.herokuapp.com/?date=2018%2F07%2F17";
