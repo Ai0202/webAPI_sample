01833d511fff02cc23bfb8306b3dd7a50a26dcb3
reeeeo <reo.117.pri@gmail.com>
2018-07-27 15:53:31 +0800

    mkdir re

---
+++ reo/resign.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_GET['name'];
+    $profile = $_GET['profile'];
+
+    $dsn = 'mysql:dbname='. getenv('DB_NAME') .';host=' . getenv('HOST_NAME');
+    $user = getenv('SQL_USER');
+    $password = getenv('SQL_PASSWORD');
+    $dbh = new PDO($dsn, $user, $password);
+    // SQL文にエラーがあった際、画面にエラーを出力する設定
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->query('SET NAMES utf8');
+
+    $sql = 'INSERT INTO `re`(`name`, `profile`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?)';
+
+    $data = [$name,$profile,Date(), Date()];
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);  // SQLインジェクション対策で作った
+
+    // ３．データベースを切断する
+    $dbh = null;
+
+    echo json_encode(true);
