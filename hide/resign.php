5b55042034e22fb89a5049f06ee0a76ebe8b1d16
秀和 <hidekazu5200@gmail.com>
2018-07-27 15:30:52 +0800

    初回

---
+++ hide/resign.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_GET['name'];
+    $animal = $_GET['animal'];
+
+    $dsn = 'mysql:dbname='. getenv('DB_NAME') .';host=' . getenv('HOST_NAME');
+    $user = getenv('SQL_USER');
+    $password = getenv('SQL_PASSWORD');
+    $dbh = new PDO($dsn, $user, $password);
+    // SQL文にエラーがあった際、画面にエラーを出力する設定
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->query('SET NAMES utf8');
+
+    $sql = 'INSERT INTO `pocari`(`name`, `coke`) VALUES (?, ?)';
+ 
+    $data = [$name,$animal];
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);  // SQLインジェクション対策で作った
+
+    // ３．データベースを切断する
+    $dbh = null;
+
+    echo json_encode(true);
+
