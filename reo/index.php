7750238410cd02964b87c27365c574c53823f0b4
reeeeo <reo.117.pri@gmail.com>
2018-07-30 14:19:55 +0800

    API修正

---
+++ reo/index.php
@@ -0,0 +1,14 @@
+<?php
+  require('dbconnect.php');
+  $sql = 'SELECT * FROM `re`';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+  $result = [];
+  while (true) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$rec) {
+      break;
+    }
+    $result[] = $rec;
+  }
+  echo json_encode($result);
--- reo/resign.php
+++ reo/resign.php
@@ -1,22 +1,8 @@
 <?php
-    $name = $_GET['name'];
-    $profile = $_GET['profile'];
-
-    $dsn = 'mysql:dbname='. getenv('DB_NAME') .';host=' . getenv('HOST_NAME');
-    $user = getenv('SQL_USER');
-    $password = getenv('SQL_PASSWORD');
-    $dbh = new PDO($dsn, $user, $password);
-    // SQL文にエラーがあった際、画面にエラーを出力する設定
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->query('SET NAMES utf8');
-
-    $sql = 'INSERT INTO `re`(`name`, `food`) VALUES (?, ?)';
-
-    $data = [$name,$profile,Date(), Date()];
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute($data);  // SQLインジェクション対策で作った
-
-    // ３．データベースを切断する
-    $dbh = null;
-
-    echo json_encode(true);
+  require('dbconnect.php');
+  $sql = 'INSERT INTO `re`(`name`, `food`) VALUES (?, ?)';
+  $data = [$_GET['name'],$_GET['food']];
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
+  $dbh = null;
+  echo json_encode(true);
