93e1f3a0d5040bebb5635f450aa9f9f13822d7f2
reeeeo <reo.117.pri@gmail.com>
2018-07-30 15:29:23 +0800

    登録api修正

--- reo/resign.php
+++ reo/resign.php
@@ -1,7 +1,9 @@
 <?php
   require('connect.php');
+  $name = $_GET['name']
+  $food = $_GET['food']
   $sql = 'INSERT INTO `re`(`name`, `food`) VALUES (?, ?)';
-  $data = [$_GET['name'],$_GET['food']];
+  $data = [$name, $food];
   $stmt = $dbh->prepare($sql);
   $stmt->execute($data);
   $dbh = null;
